ce51667b0bb24af61131b020ab5b573efa175fd7
Meekl-e <108610643+Meekl-e@users.noreply.github.com>
2023-04-15 20:11:50 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from Perceptron import *
 
-p = Perceptron("sets and data/nouns.txt","sets and data/verbsFixed.txt", 10, -1) # 100 - количество итерация, 1000 - количество слов в общей выборке
+p = Perceptron("sets and data/nouns.txt","sets and data/verbsFixed.txt", 10, -1) #  количество итерация, количество слов в общей выборке (-1 - все слова)
 while True:
     print(p.test(input()))
 
